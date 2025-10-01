998cedce6a5372f71b2fe05261f8206d27fba139
Damien Lebon <damienlebon.ifpa@gmail.com>
2025-10-01 20:20:26 +0200

    update style

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    content: [
+        './templates/**/*.twig',
+        './assets/**/*.js',
+        './assets/styles/**/*.css',
+    ],
+    theme: { extend: {} },
+    plugins: [],
+};
